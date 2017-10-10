70805cbb4f3a5c72627d43811b6a213da122f208
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-10 15:17:26 -0700

    version and release bump.

--- version.php
+++ version.php
@@ -6,7 +6,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017071700;
+$plugin->version   = 2017101000;
 $plugin->requires  = 2016112900;
 $plugin->component = 'local_iliosapiclient';
-$plugin->release   = '1.0.0';
+$plugin->release   = '1.1.0';
