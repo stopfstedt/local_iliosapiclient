47406c7752e6a01bc44bd7241304e9ce1cba5887
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-12-12 14:49:08 -0800

    version and requirement bump to Moodle 4.1

--- version.php
+++ version.php
@@ -6,6 +6,6 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022103100;
-$plugin->requires  = 2022041200;
+$plugin->version   = 2022121200;
+$plugin->requires  = 2022111800;
 $plugin->component = 'local_iliosapiclient';
